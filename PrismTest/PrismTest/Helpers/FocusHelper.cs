b71e356e6996d2dc08f5ee7d2480474d088b29cc
adaml984 <adam.lamarz@outlook.com>
2017-03-27 13:35:31 +0200

    added missig file

---
+++ PrismTest/PrismTest/Helpers/FocusHelper.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace PrismTest.Helpers
+{
+	public static class FocusHelper
+	{
+		public static readonly BindableProperty FocusedElementProperty =
+			BindableProperty.CreateAttached(
+				"FocusedElement",
+				typeof(bool),
+				typeof(BindableObject),
+				false,
+				propertyChanged: OnPropertyChanged);
+
+		public static void SetFocusedElement(View view, bool shouldFocus)
+		{
+			if (view != null && shouldFocus)
+				view.Focus();
+		}
+
+		public static bool GetFocusedElement(View view) { return view?.IsFocused ?? false; }
+
+		private static void OnPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+		{
+			if (newvalue == null)
+				return;
+			SetFocusedElement(bindable as View, (bool) newvalue);
+		}
+	}
+}
